96a2aae8a78f0bca5877a8a1090a75b22d3884b5
Trần Tuấn Anh <ttanh150703@gmail.com>
2024-06-11 10:01:56 +0700

    update main code to use recursion

--- src/main/java/com/microsoft/mathutil/core/MathUtility.java
+++ src/main/java/com/microsoft/mathutil/core/MathUtility.java
@@ -30,13 +30,21 @@ public class MathUtility {
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid n. n must be between 0 to 20");
         }
-        if (n == 0) {
+        if (n == 0 || n == 1) {
             return 1;
         }
-        for (int i = 1; i <= n; i++) {
-            product *= i;
-        }
+       
 
-        return product;
+        return n * getFactorial(n - 1);
+        //recursion - đệ quy - gọi lại chính mình với 1 quy mô khác
+        
     }
 }
+
+//5! = 1.2.3.4.5
+//   = 5 x 4!
+//4! = 4 x 3!
+//3! = 3 x 2!
+//2! = 2 x 1!
+//1! = 1
+// n! = n x (n - 1)!
